273d095b1d34da8f2aaa571f1e1318f084bd62c3
jacab <j.a.cabrerapuente@gmail.com>
2018-10-31 18:32:14 -0600

    version halloween

--- Form1.cs
+++ Form1.cs
@@ -11,11 +11,17 @@ using System.Windows.Forms;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net;
+using ChatBot_IDE.Logica;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace ChatBot_IDE
 {
     public partial class Form1 : Form
     {
+        List<string> impresiones;
+        List<ErrorC> errores;
+
         public Form1()
         {
             InitializeComponent();
@@ -47,7 +53,7 @@ namespace ChatBot_IDE
         {
             RichTextBox arch = new RichTextBox();
             arch.Size = new Size(this.archivos.Width - 10, this.archivos.Height - 10);
-            TabPage pagina = new TabPage("Nuevo.cbc");
+            TabPage pagina = new TabPage();
             pagina.Controls.Add(arch);
             this.archivos.Controls.Add(pagina);
         }
@@ -60,19 +66,33 @@ namespace ChatBot_IDE
 
         private async void ejecutarToolStripMenuItem_ClickAsync(object sender, EventArgs e)
         {
+            impresiones = new List<string>();
+            errores = new List<ErrorC>();
+            consolaSalidas.Text = "";
+
             int actual = this.archivos.SelectedIndex;
             String texto = ((RichTextBox)((this.archivos.Controls[actual]).Controls[0])).Text;
 
-            string json = "\"" + texto + "\"";
+            string json = texto;
             var buffer = System.Text.Encoding.UTF8.GetBytes(json);
-
             var byteContent = new ByteArrayContent(buffer);
-
             byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
             var response = await client.PostAsync("https://localhost:44375/api/ComunicacionIDE", byteContent);
 
             var responseString = await response.Content.ReadAsStringAsync();
+
+            string jsonR = Convert.ToString(responseString);
+
+            JObject obj = JObject.Parse(jsonR);
+            JToken uno = obj["Impresiones"];
+            JToken dos = obj["Errores"];
+            string one = uno.ToString();
+            string two = dos.ToString();
+            JArray cadenas = JArray.Parse(one);
+            JArray errs = JArray.Parse(two);
+            extraerImpresiones(cadenas);
+            extraerErrores(errs);
         }
 
         private void verErroresToolStripMenuItem_Click(object sender, EventArgs e)
@@ -87,7 +107,48 @@ namespace ChatBot_IDE
 
         private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            int actual = this.archivos.SelectedIndex;
+            String texto = ((RichTextBox)((this.archivos.Controls[actual]).Controls[0])).Text;
+            SaveFileDialog guardar = new SaveFileDialog();
+            if (guardar.ShowDialog() == DialogResult.OK)
+            {
+                String file = guardar.FileName;
+                File.WriteAllText(file, texto);
+            }
+        }
 
+        private void extraerImpresiones(JArray impres) 
+        {
+            foreach (JObject impresion in impres)
+            {
+                string actual = Convert.ToString(impresion.GetValue("valor"));
+                consolaSalidas.Text += actual + "\n";
+                impresiones.Add(actual);
+            }
+        }
+
+        private void extraerErrores(JArray errs)
+        {
+            ErrorC auxiliar;
+            foreach (JObject error in errs)
+            {
+                auxiliar = new ErrorC();
+                if (Convert.ToString(error.GetValue("tipo")).Equals("Semantico"))
+                {
+                    auxiliar.tipo = "Semantico";
+                    auxiliar.descripcion = Convert.ToString(error.GetValue("descripcion"));
+                    errores.Add(auxiliar);
+                }
+                else
+                {
+                    auxiliar.tipo = Convert.ToString(error.GetValue("tipo"));
+                    auxiliar.descripcion = Convert.ToString(error.GetValue("descripcion"));
+                    auxiliar.lexema = Convert.ToString(error.GetValue("lexema"));
+                    auxiliar.fila = Convert.ToInt32(error.GetValue("fila"));
+                    auxiliar.columna = Convert.ToInt32(error.GetValue("columna"));
+                    errores.Add(auxiliar);
+                }
+            }
         }
     }
 }
---
+++ Logica/ErrorC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot_IDE.Logica
+{
+    class ErrorC
+    {
+        public string tipo;
+        public int fila;
+        public int columna;
+        public string lexema;
+        public string descripcion;
+
+        //constructor para un lexico y un sintactico
+        public ErrorC(string tipo, int fila, int columna, string lexema, string descripcion) {
+            this.tipo = tipo;
+            this.fila = fila;
+            this.columna = columna;
+            this.lexema = lexema;
+            this.descripcion = descripcion;
+        }
+
+        //constructor para un error semantico
+        public ErrorC(string tipo, string descripcion) {
+            this.tipo = tipo;
+            this.descripcion = descripcion;
+            this.fila = 0;
+            this.columna = 0;
+            this.lexema = "";
+        }
+
+        public ErrorC()
+        {
+            tipo = null;
+            descripcion = null;
+            fila = 0;
+            columna = 0;
+            lexema = "";
+        }
+    }
+}
